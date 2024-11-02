4d895032d0b86c1ec3c137c52517ef86ea9703e4
Tygarinn <ivang3422@gmail.com>
2024-11-02 17:43:55 +0200

    Task 5 completed

---
+++ Lesson_1/Task_5/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int height;
+
+    cout << "Enter Christmas tree height: ";
+    cin >> height;
+
+    for (int row = 1; row <= height; ++row){
+
+        for (int spaces = 0; spaces < height - row; ++spaces) {
+            cout << " ";
+        }
+
+        for (int stars = 0; stars < (2 * row - 1); ++stars) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+    for (int row = 0; row < height - 1; ++row) {
+        cout << " ";
+    }
+    cout << "*" << endl;
+
+    return 0;
+}
