afaa382cb6a7e6d6f65f8c222ade35b7d09d83b1
Tygarinn <ivang3422@gmail.com>
2024-12-09 16:32:16 +0200

    Lesson 2, task 1 updated with using cycles

--- Lesson_2/Task_1/main.cpp
+++ Lesson_2/Task_1/main.cpp
@@ -8,18 +8,16 @@ int main()
     SetConsoleOutputCP(CP_UTF8);
 
     int arr[5];
-    cout << "Please, enter 5 numbers" << endl;
+    cout << "Please, enter 5 numbers:" << endl;
 
-    cin >> arr[0];
-    cin >> arr[1];
-    cin >> arr[2];
-    cin >> arr[3];
-    cin >> arr[4];
+    for (int counter = 0; counter < 5; ++counter) {
+        cin >> arr[counter];
+    }
 
-    cout << arr[0] << ",";
-    cout << arr[1] << ",";
-    cout << arr[2] << ",";
-    cout << arr[3] << ",";
-    cout << arr[4];
-
+    for (int counter = 0; counter< 5; ++counter) {
+        cout << arr[counter];
+        if (counter < 4) {
+            cout << ",";
+        }
+    }
 }
